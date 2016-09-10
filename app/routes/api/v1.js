79c8f1fbd5eda5a8c2248917c01702a6406090f7
Aniruddha Chakraborty <achakraborty510@gmail.com>
2016-09-10 23:56:38 +0600

    added new changes , updated queries , match queries added

--- app/routes/api/v1.js
+++ app/routes/api/v1.js
@@ -13,17 +13,72 @@ module.exports = function (app, neo4j,mongoose,express) {
 
 	var api = express.Router();
 
+
+/**
+ *  GET all subjects
+ */
 	api.get('/all-subjects',(req,res) => {
 
+		neo4j.cypher({
+
+		    query: 'MATCH(n:subject) RETURN n'
+
+		}, function (err,results) {
+
+		    if (err)
+		    	throw err;
+
+		    console.log(results);
+
+
+		});
+
 
 	});
 
+/**
+ *  GET courses using subject id 
+ */
 	api.get('/courses/:subject_id',(req,res) => {
 
+		neo4j.cypher({
+
+			    query: 'MATCH(n:course{subject_code:{subject_code}}) RETURN n',
+			    params: {
+
+			    	subject_code:subject_code
+			    },
+
+			}, function (err,results) {
+
+			    if (err)
+			    	throw err;
+
+			    console.log(results);
+
+			});
 
 	});
 
-	api.get('/course-info/:course:id',(req,res) => {
+	api.get('/course-info/:course_id',(req,res) => {
+
+
+		neo4j.cypher({
+
+			    query: 'MATCH(n:course{object_id:{object_id}}) RETURN n',
+			    params: {
+
+			    	object_id:course_id
+			    },
+
+			}, function (err,results) {
+
+			    if (err)
+			    	throw err;
+
+			    console.log(results);
+
+			});
 
 
 	});
@@ -123,51 +178,51 @@ module.exports = function (app, neo4j,mongoose,express) {
 
 						Subject.find({ subject_code: subject_code },function(err,info){
 
-								info.forEach(function(info){
+								info.forEach(function(info) {
 
-										let course_name = info.subject_name;
-										let course_code = info.course_code;
-										let course_credit = info.course_credit;
+								let course_name = info.subject_name;
+								let course_code = info.course_code;
+								let course_credit = info.course_credit;
+
+									neo4j.cypher({
+
+									    query: "CREATE (n:course{name:{course_name},course_code:{course_code},course_credit:{course_credit},object_id:{object_id},subject_code:{subject_code}}) RETURN n",
+									    params: {
+									       	object_id: ObjectID(),
+									       	course_name: course_name,
+									        course_code: course_code,
+									        course_credit: course_credit,
+									        subject_code: subject_code
+									    },
+
+									}, function (err,results) {
+
+									    if (err)
+									    	throw err;
+
+									    let course_object_id = results[0]['n']['properties']['object_id'];
+									  
 
 											neo4j.cypher({
 
-											    query: "CREATE (n:course{name:{course_name},course_code:{course_code},course_credit:{course_credit},object_id:{object_id},subject_code:{subject_code}}) RETURN n",
+											    query: "MATCH(n:subject{object_id: {subject_object_id}}),(m:course{object_id:{course_object_id}}) CREATE (n)-[r:Course]->(m)",
 											    params: {
-											       	object_id: ObjectID(),
-											       	course_name: course_name,
-											        course_code: course_code,
-											        course_credit: course_credit,
-											        subject_code: subject_code
-											    },
 
-											}, function (err,results) {
+											       	subject_object_id: subject_object_id,
+											       	course_object_id:course_object_id
+											    }
+
+											}, function (err) {
 
 											    if (err)
 											    	throw err;
 
-											    let course_object_id = results[0]['n']['properties']['object_id'];
-											  
-
-													neo4j.cypher({
-
-													    query: "MATCH(n:subject{object_id: {subject_object_id}}),(m:course{object_id:{course_object_id}}) CREATE (n)-[r:Course]->(m)",
-													    params: {
-
-													       	subject_object_id: subject_object_id,
-													       	course_object_id:course_object_id
-													    }
-
-													}, function (err) {
-
-													    if (err)
-													    	throw err;
-
-
-													});
-
-
 
 											});
+
+
+
+									});
 
 
 
@@ -175,89 +230,11 @@ module.exports = function (app, neo4j,mongoose,express) {
 
 						});
 
-
-/*
-
-
-							neo4j.cypher({
-
-							    query: "MATCH(n:subject{object_id: {subject_object_id}}),(m:course{object_id:{course_object_id}}) CREATE (n)-[r:Course]->(m)",
-							    params: {
-
-							       	object_id: ObjectID(),
-							       	subject_name: subjects[n],
-							        subject_code: '0'+n
-							    },
-
-							}, function (err,results) {
-
-							    if (err)
-							    	throw err;
-
-							    let code = results[0]['n']['properties']['object_id'];
-
-
-
-							    	console.log(code);
-
-
-							});
- */
-
-
-				});
-
-		}
- 
- 
-
-
-/*
-
-		var subjects = ['BBA','CSIT','CSE','Textile','EEE','English','Economics','Environment','LL. B'];
-
-		for (var i = 1; i <= 9; i++) {
-
-				neo4j.cypher({
-
-				    query: 'CREATE (n:subject {object_id:{object_id}, name: {subject_name} , subject_code: {subject_code} })',
-				    params: {
-
-				       	object_id: ObjectID(),
-				       	subject_name: subjects[i],
-				        subject_code: '0'+i
-				    },
-
-				}, function (err) {
-
-				    if (err)
-				    	throw err;
-
 				});
 
 		}
 
-			neo4j.cypher({
-
-			    query: 'CREATE (n:course { object_id:{object_id}, name: {course_name} , course_code: {course_code} })',
-			    params: {
-			    	object_id: ObjectID(),
-			        course_name: 'C programming',
-			        course_code: 'CSE 101'
-			    },
-
-			}, function (err) {
-
-			    if (err)
-			    	throw err;
-
-			});
-
- */
-
-
-
-			return res.send();
+		return res.send();
 
 	});
 
@@ -266,35 +243,6 @@ module.exports = function (app, neo4j,mongoose,express) {
 
 			// relationship query
 
-			neo4j.cypher({
-
-					query: "MATCH (n) RETURN n"
-
-			},function(err,results){
-
-					if (err)
-						throw err;
-
-					results.forEach(function(results){
-
-							var data = results['n']['properties'];
-							console.log(data['object_id']);
-
-
-								neo4j.cypher({
-
-										query:"MATCH(n:{object_id:{object_id}}),(m:{object_id:{}})"
-
-								});
-
-
-
-
-					});
-
-			});
-
-			return res.send();
 	});
 
 
